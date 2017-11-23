388918e3330f24fc6ca1ea10929a47106aea5e0a
Celine Lee <celinl@student.cis.edu.hk>
2017-11-23 12:48:35 +0800

    HI

---
+++ HtmlValidator.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class HtmlValidator
+{
+	public HtmlValidator()
+	{
+		Queue<HtmlTag> q = new ArrayList<HtmlTag>();
+	}
+
+	public HtmlValidator (Queue<HtmlTag> tags)
+	{
+		Queue<HtmlTag> q = new ArrayList<HtmlTag>(tags);
+	}
+
+	public void addTag (HtmlTag tag)
+	{
+		
+	}
+}

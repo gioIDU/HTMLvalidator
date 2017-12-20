efdb584f614bac721bbbf9d9f22d66867e196d01
giol <giol@student.cis.edu.hk>
2017-12-20 20:06:51 +0900

    Merged Gio's code with Celine's code

--- src/HtmlValidatorTest.java
+++ src/HtmlValidatorTest.java
@@ -5,17 +5,67 @@
 import java.util.*;
 
 public class HtmlValidatorTest {
-	public static void main(String[] args) {
-		// <b>Hi</b><br/>
-		// A Queue of tags you may modify and pass to your HtmlValidator object
-		Queue<HtmlTag> tags = new LinkedList<HtmlTag>();
+    //	public static void main(String[] args) {
+//		// <b>Hi</b><br/>
+//		// A Queue of tags you may modify and pass to your HtmlValidator object
+//		Queue<HtmlTag> tags = new LinkedList<HtmlTag>();
+//
+//		tags.add(new HtmlTag("html", false));
+//
+//
+//		HtmlValidator test = new HtmlValidator(tags);
+////		test.removeAll("p");
+//		test.validate();
+//		// Your code here
+//	}
+    
+    public static void main(String[] args) {
+        run("<body> <p> <i> </i> </p> </body>", "<body> <p> <i> </i> </p> </body>", "b");
 
-		tags.add(new HtmlTag("html", false));
+        run("<b> </b> <b> </b>", "", "b");
+
+        run("", "", "b");
+    }
+
+    public static void run(String t, String e, String r) {
+        Queue<HtmlTag> tags = HtmlTag.tokenize(t);
+        Queue<HtmlTag> expected = HtmlTag.tokenize(e);
+
+        HtmlValidator h = new HtmlValidator(tags);
+
+        try {
+            h.removeAll(r);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Caught Illegal Argument.");
+        }
+
+        Queue<HtmlTag> result = h.getTags();
+
+        boolean valid = true;
+
+        int size = expected.size();
+
+        for (int i = 0; i < size; i++) {
+            HtmlTag cure = expected.poll();
+            HtmlTag curt = result.poll();
+            if (!cure.equals(curt))
+                valid = false;
+            result.offer(curt);
+            expected.offer(cure);
+        }
+
+        if (h.getTags().size() != expected.size())
+            valid = false;
 
 
-		HtmlValidator test = new HtmlValidator(tags);
-//		test.removeAll("p");
-		test.validate();
-		// Your code here
-	}
+        if (valid)
+            System.out.println("Test Passed");
+        else {
+            System.out.println("Test Failed");
+            System.out.println("  Result: " + result);
+            System.out.println("Expected: " + expected);
+        }
+        System.out.println("");
+
+    }
 }
